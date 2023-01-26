7bf123d8ad7eed66a195db32db1cbecfde0f6016
Caleb Hodges <chodgesdev@gmail.com>
2023-01-25 20:26:58 -0600

    Step 4 complete

--- server/controller.js
+++ server/controller.js
@@ -49,5 +49,14 @@ module.exports = {
         ON u.user_id = c.user_id`)
         .then((dbRes => res.status(200).send(dbRes[0])))
         .catch((err) => console.log(err));
+    },
+
+    getPendingAppointments: (req, res) => {
+        sequelize.query(`SELECT * FROM cc_appointments
+        WHERE approved = false
+        ORDER BY date DESC`)
+        .then((dbRes) => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
     }
+ 
 }
